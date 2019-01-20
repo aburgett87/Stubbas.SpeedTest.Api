071a34d37845a045813371ba5241c8a58facecc4
Alexander Burgett <4073978+aburgett87@users.noreply.github.com>
2019-01-20 15:33:13 +1100

    removing cd commands from build.sh

--- build.sh
+++ build.sh
@@ -1,10 +1,9 @@
 dotnet restore
 dotnet test ./test/Stubias.SpeedTest.Api/Stubias.SpeedTest.Api.Tests.csproj
 dotnet test ./test/Stubias.SpeedTest.Api.Integration/Stubias.SpeedTest.Api.Integration.csproj
-cd ./src/Stubias.SpeedTest.Api
 dotnet lambda package \
     --disable-version-check true \
     --configuration release \
     --framework netcoreapp2.1 \
-    --output-package bin/local/release/netcoreapp2.1/deploy-package.zip
-cd ../..
+    --output-package bin/local/release/netcoreapp2.1/deploy-package.zip \
+    --project-location ./src/Stubias.SpeedTest.Api/Stubias.SpeedTest.Api.csproj
